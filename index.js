7de18df490b04a720e10cb4f82cdb106d1224446
sudocyberghost <sudoyberghost@gmail.com>
2023-02-23 01:27:29 +0300

    fix the scroll bar when it aside

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const openMenu = () => {
     document.querySelector(".backdrop").className = 'backdrop active';
     document.querySelector("aside").className = 'active';
+    document.querySelector("body").className = 'active';
 
 }
 const closeMenu = () => {
     document.querySelector(".backdrop").className = 'backdrop';
     document.querySelector("aside").className = '';
+    document.querySelector("body").className = '';
 
 }
 document.getElementById("menuBtn").onclick = e => {
